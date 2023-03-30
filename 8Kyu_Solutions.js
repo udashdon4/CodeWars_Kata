f77cfda88abbcdb2b99437fae13afad80d836207
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-03-30 00:33:05 -0400

    Update 8Kyu_Solutions.js
    Added S4

--- 8Kyu_Solutions.js
+++ 8Kyu_Solutions.js
@@ -31,3 +31,16 @@ should change to.For example, when the input is green, output should be yellow.
 function updateLight(current) {
     return current === "green" ? "yellow" : current === "yellow" ? "red" : "green"
   }
+
+/*___________________________________________________________________________
+### 3 ###
+Create a function that accepts a string and a single character, and returns an
+integer of the count of occurrences the 2nd argument is found in the first one.
+If no occurrences can be found, a count of 0 should be returned.
+("Hello", "o")  ==>  1
+("Hello", "l")  ==>  2
+("", "z")       ==>  0
+*/
+function strCount(str, letter){  
+    return str.split('').filter(n => n.includes(letter)).length 
+  }
