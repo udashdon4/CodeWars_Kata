0bf0c580af18f07af4593eb078fc8acb48b92194
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-03-30 00:13:08 -0400

    Update 8Kyu_Solutions.js
    Second Commit

--- 8Kyu_Solutions.js
+++ 8Kyu_Solutions.js
@@ -1,3 +1,20 @@
 /*
-My First Code Push
+Sim Dash @Udashdon4
+My solution code for 8kyu challenges (Kata)
+CodeWars
+
+Format:
+1. Challenge (Kata)
+2. Solution
 */
+
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+function positiveSum(arr) {
+    let sum = 0
+    arr.forEach (n => {n > 0 ? sum +=n : 0 })
+    return sum                   
+  }
