6673160e0f0452d7a08b388cf1c6d0c43974e592
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-04-06 01:18:39 -0400

    Update 8Kyu_Solutions 11-20.js
    Added 12th kata

--- JavaScript/8Kyu_Solutions 11-20.js
+++ JavaScript/8Kyu_Solutions 11-20.js
@@ -23,8 +23,26 @@ function fakeBin(x){
 
 /*___________________________________________________________________________
 ### 12 ###
-
+Write function bmi that calculates body mass index (bmi = weight / height2).
+if bmi <= 18.5 return "Underweight"
+if bmi <= 25.0 return "Normal"
+if bmi <= 30.0 return "Overweight"
+if bmi > 30 return "Obese"
 */
+function bmi(weight, height) {
+    let bmindex = weight/(height * height)
+    if (bmindex <= 18.5) {
+      return "Underweight"
+    }
+    else if (bmindex <= 25.0) {
+      return "Normal"
+    }
+    else if (bmindex <= 30.0) {
+      return "Overweight"
+    }
+    else {
+      return "Obese"
+}
 
 
 /*___________________________________________________________________________
