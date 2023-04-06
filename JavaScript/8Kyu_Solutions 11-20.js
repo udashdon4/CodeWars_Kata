8fa43c9d8e2d4e96114b3eae80879b226703702a
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-04-05 21:04:06 -0400

    Solutions 11-20
    Adding additional file

---
+++ JavaScript/8Kyu_Solutions 11-20.js
@@ -0,0 +1,20 @@
+/*
+Sim Dash @Udashdon4
+My solution code for challenges (Kata)
+Language: JavaScript
+CodeWars Level: 8Kyu
+Count: 11-20
+
+Format:
+1. Challenge (Kata)
+2. Solution
+*/
+
+/*___________________________________________________________________________
+### 11 ###
+You get an array of numbers, return the sum of all of the positives ones.
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+
