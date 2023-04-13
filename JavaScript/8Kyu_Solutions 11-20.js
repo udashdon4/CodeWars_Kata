06af63d5fa160c0b670bd7a8d1a36d480ee0404f
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-04-13 02:07:58 -0400

    Update 8Kyu_Solutions 11-20.js

--- JavaScript/8Kyu_Solutions 11-20.js
+++ JavaScript/8Kyu_Solutions 11-20.js
@@ -60,8 +60,22 @@ function highAndLow(numbers){
 
 /*___________________________________________________________________________
 ### 14 ###
+Is the string uppercase?
+Task
 
+Create a method to see whether the string is ALL CAPS.
+Examples (input -> output)
+
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
 */
+String.prototype.isUpperCase = function() {
+  return this == this.toUpperCase()
+}
     
 /*___________________________________________________________________________
 ### 15 ###
