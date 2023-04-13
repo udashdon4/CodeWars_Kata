becc168a65d4f7fbb75d78daabf80e16249cd39e
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-04-12 22:08:13 -0400

    Update 8Kyu_Solutions 11-20.js
    Added 13th Kata

--- JavaScript/8Kyu_Solutions 11-20.js
+++ JavaScript/8Kyu_Solutions 11-20.js
@@ -47,12 +47,25 @@ function bmi(weight, height) {
 
 /*___________________________________________________________________________
 ### 13 ###
-
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+Examples
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
 */
+function highAndLow(numbers){
+  let arr1 = numbers.split(' ').sort((a,b) => a - b)
+  return `${arr1[arr1.length-1]} ${arr1[0]}`
+}
 
 
 /*___________________________________________________________________________
 ### 14 ###
+
+*/
+
+/*___________________________________________________________________________
+### 15 ###
 
 */
 
