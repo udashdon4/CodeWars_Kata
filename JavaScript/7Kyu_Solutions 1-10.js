cc4e425d23f6de521649f0d7e83d8a8e41eec50b
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-04-24 23:26:36 -0400

    Update 7Kyu_Solutions 1-10.js
    Added 9th Kata

--- JavaScript/7Kyu_Solutions 1-10.js
+++ JavaScript/7Kyu_Solutions 1-10.js
@@ -147,3 +147,17 @@ function sumTwoSmallestNumbers(numbers) {
 
 /*___________________________________________________________________________
 ### 9 ###
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+Examples: (Input --> Output)
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+*/
+var number=function(array){
+  let result = []
+  for(let i = 1; i <= array.length; i++){
+    result.push(`${i}: ${array[i-1] }`)
+  }
+  return result
+}
