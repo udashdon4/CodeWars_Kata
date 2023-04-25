1c4e96ccadbbe1d31a977453a5ad3791fddba6ec
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-04-24 22:48:52 -0400

    Update 7Kyu_Solutions 1-10.js

--- JavaScript/7Kyu_Solutions 1-10.js
+++ JavaScript/7Kyu_Solutions 1-10.js
@@ -109,8 +109,20 @@ function validatePIN (pin) {
 
 /*___________________________________________________________________________
 ### 7 ###
-
+Sometimes there are problems: lack of colors, technical malfunction and a "bad" 
+control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+You have to write a function printer_error which given a string will return the 
+error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+The string has a length greater or equal to one and contains only letters from ato z.
+Examples:
+s="aaabbbbhaijjjm"
+printer_error(s) => "0/14"
+s="aaaxbbbbyyhwawiwjjjwwm"
+printer_error(s) => "8/22"
 */
+function printerError(s) {
+  return `${s.replace(/[^n-zN-Z]/g, '').length}/${s.length}`
+}
 
 /*___________________________________________________________________________
 ### 8 ###
