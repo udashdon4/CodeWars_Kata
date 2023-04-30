6da5a9bb1931f3dfad776fada65a5c684a2ffd50
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-04-29 21:24:32 -0400

    Create 7Kyu_Solutions 11-20.js

---
+++ JavaScript/7Kyu_Solutions 11-20.js
@@ -0,0 +1,17 @@
+/*
+Sim Dash @Udashdon4
+My solution code for challenges (Kata)
+Language: JavaScript
+CodeWars Level: 7Kyu
+Count: 11-20
+
+Format:
+1. Challenge (Kata)
+2. Solution
+*/
+
+/*___________________________________________________________________________
+### 11 ###
+
+*/
+
