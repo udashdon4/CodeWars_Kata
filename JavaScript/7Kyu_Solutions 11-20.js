7a0c97724327e2ca9882c39bd2725c94c5fb301d
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-04-29 23:06:14 -0400

    Update 7Kyu_Solutions 11-20.js
    Added 11th Kata

--- JavaScript/7Kyu_Solutions 11-20.js
+++ JavaScript/7Kyu_Solutions 11-20.js
@@ -12,7 +12,29 @@ Format:
 
 /*___________________________________________________________________________
 ### 11 ###
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+Note: anagrams are case insensitive
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+Examples
+    "foefet" is an anagram of "toffee"
+    "Buckethead" is an anagram of "DeathCubeK"
+*/
+var isAnagram = function(test, original) {
+
+    let teSorted = test.toLowerCase().split('').sort()
+    let orSorted = original.toLowerCase().split('').sort()
+    let result = 0
+    
+    if (teSorted.length === orSorted.length) {
+    for(let i = 0; i < teSorted.length; i++){
+      result += teSorted[i] === orSorted[i] ? 1 : 0
+      }
+     }
+    return result === orSorted.length
+  };
+
+/*___________________________________________________________________________
+### 12 ###
 
 */
-
 
