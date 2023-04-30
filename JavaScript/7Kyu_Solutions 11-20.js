72acc58855e1c798b26df79735c48d47ad952c80
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-04-29 23:14:19 -0400

    Update 7Kyu_Solutions 11-20.js
    Added 12th Kata.

--- JavaScript/7Kyu_Solutions 11-20.js
+++ JavaScript/7Kyu_Solutions 11-20.js
@@ -35,6 +35,20 @@ var isAnagram = function(test, original) {
 
 /*___________________________________________________________________________
 ### 12 ###
-
+Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+Examples:
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
 */
+function roundToNext5(n){
+    return Math.ceil((n/5))*5
+  }
 
