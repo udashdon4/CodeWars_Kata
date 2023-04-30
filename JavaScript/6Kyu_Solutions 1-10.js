d02d0c989ab3ce957a3dad38a233b0f0d756ffc3
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-04-29 21:21:58 -0400

    Create 6Kyu_Solutions 1-10.js
    Created 6Kyu solutions file

---
+++ JavaScript/6Kyu_Solutions 1-10.js
@@ -0,0 +1,22 @@
+/*
+Sim Dash @Udashdon4
+My solution code for challenges (Kata)
+Language: JavaScript
+CodeWars Level: 6Kyu
+Count: 1-10
+
+Format:
+1. Challenge (Kata)
+2. Solution
+*/
+
+/*___________________________________________________________________________
+### 1 ###
+
+*/
+
+
+/*___________________________________________________________________________
+### 2 ###
+
+*/
