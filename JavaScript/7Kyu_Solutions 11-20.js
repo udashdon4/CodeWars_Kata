22bb12823dfd047e01a817029fe38fcbfc472f7e
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-05-01 23:10:51 -0400

    Update 7Kyu_Solutions 11-20.js
    Added 16th Kata

--- JavaScript/7Kyu_Solutions 11-20.js
+++ JavaScript/7Kyu_Solutions 11-20.js
@@ -114,12 +114,27 @@ var isSquare = function(n){
   return n === 0 ? true : n % Math.sqrt(n) == 0 ? true : false
 }
 
-
-
 /*___________________________________________________________________________
 ### 16 ###
+In mathematics, the factorial of a non-negative integer n, denoted by n!, is 
+the product of all positive integers less than or equal to n. For example: 
+5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
 
+Write a function to calculate factorial for a given input. If input is below 0 or above 12
+throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException
+ (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
 */
+function factorial(n) {
+  let fac = 1
+  if (n < 0 || n > 12) {
+    throw new RangeError
+  }
+  for (let i =1; i <= n; i ++) {
+    fac *=i
+  }
+  return fac
+}
+
 
 /*___________________________________________________________________________
 ### 17 ###
